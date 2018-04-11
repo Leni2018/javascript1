b6621a7ee7e222366eb97c167bbeb034b7592d50
Marlene Weiss <marlene.weiss@student.uibk.ac.at>
2018-04-11 17:47:50 +0200

    Datentypen abgeschlossen

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ console.log(meineNachricht);
 
 //für Zeilenumbrüche ``, oder, um innerhalb der `` "" zu schreiben (s.u.)
 meineNachricht = `Ein mehr
-une ein ganz langer 
+und ein ganz langer 
 text
 als zeichnketten
 jipiii`;
@@ -35,5 +35,28 @@ console.log("über18?", isOver18)
 let age=19;
 console.log("age über 18", age > 18);
 
+//Datentyp Zahlenliste. ! Einzelnen Elemente der Liste mit , trennen!
+// namensListe [1] bezeihnet immer das 2. Element > das 1. ist 0
+let zahlenListe = [2, 11, 24, 33];
+console.log(zahlenListe);
+let namensListe = ["Jane", "John"];
+console.log(namensListe[1]); 
+
+// zeigt Lände der Liste an
+console.log("Anzahl der Namen: ", namensListe.length);
+
+// Datentyp Objekt ! , zwischen den einzelnen Elementen. In der Konsole können diese Infos aufgeklappt werden
+let benutzer = {
+    vorname: "Jane",
+    nachname: "Doe",
+    alter: 28
+};
+console.log("Benutzer Alter: ", benutzer.alter);
+
+// Bestzeit wird dann noch hinzugefügt
+benutzer.bestzeit = 200;
+console.log(benutzer);
+benutzer ["bestzeit ever"] = 100;
+console.log(benutzer);
 
 
