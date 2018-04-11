65674ddfda649d59abf95a9baa4568ecf4f14016
Marlene Weiss <marlene.weiss@student.uibk.ac.at>
2018-04-11 17:25:48 +0200

    Änderungen im JS

---
+++ main.js
@@ -0,0 +1,39 @@
+//alert("Hello world");
+
+//let leitet Variablen ein > Name einer Variablen kann keine Leerzeichen & - enthalten, darf nicht mit Zahl anfangen; Wenn Variable Zahlen sind, geht’s auch ohne „“ 
+let nachricht = "Hallo Welt";
+//alert(nachricht);
+nachricht = "neuer text";
+//alert (nachricht);
+console.log (nachricht)
+const linkColor = "#ff0000";
+let eine_variable = "pips";
+console.log("Variable: eine_variable: " , eine_variable);
+
+//numerische Datentypen:
+let highScore = 75.9944;
+console.log("Highscore ist: ", highScore / 20);
+
+//Datentypen Zeichenkette
+let meineNachricht = "test1";
+console.log(meineNachricht);
+meineNachricht = "text2";
+console.log(meineNachricht);
+
+//für Zeilenumbrüche ``, oder, um innerhalb der `` "" zu schreiben (s.u.)
+meineNachricht = `Ein mehr
+une ein ganz langer 
+text
+als zeichnketten
+jipiii`;
+
+let fullName = `Jonny "best dude ever" Lebowsky`;
+console.log(fullName)
+
+let isOver18 = true;
+console.log("über18?", isOver18)
+let age=19;
+console.log("age über 18", age > 18);
+
+
+
